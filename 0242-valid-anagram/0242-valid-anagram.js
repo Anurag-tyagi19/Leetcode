e0d28f810c37df9d9cc8da863e6a9cde1d5bbc91
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-06 00:55:57 +0530

    Time: 17 ms (39.88%), Space: 60.9 MB (13.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -6,7 +6,16 @@
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
 
-    return s.split("").sort().join("") ===
-           t.split("").sort().join("");
+    let map = {};
 
+    for (let ch of s) {
+        map[ch] = (map[ch] || 0) + 1;
+    }
+
+    for (let ch of t) {
+        if (!map[ch]) return false;
+        map[ch]--;
+    }
+
+    return true;
 };
