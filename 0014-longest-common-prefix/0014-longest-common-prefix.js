8e362a56b2f5f5038b2186ee6c3c6dcd832364fb
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-06 00:42:45 +0530

    Time: 1 ms (58.85%), Space: 54.3 MB (54.26%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let str = strs.sort()
+    let result = ""
+    let str1 = str[0]
+    let str2 = str[str.length-1]
+    for(i=0;i<str1.length;i++){
+        if(str1[i]!== str2[i]){
+            break
+        }
+        result = result + str1[i]
+    }
+
+    return result
+
+};
