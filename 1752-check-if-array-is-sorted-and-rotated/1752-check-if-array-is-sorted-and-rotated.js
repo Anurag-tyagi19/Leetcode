82cacbd66a768919b7776065dcaec65e52ff41f3
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-14 23:07:05 +0530

    Time: 0 ms (100%), Space: 53 MB (82.37%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    if(nums.length ==0 || nums.length==1) return true
+    let count = 0
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[i+1]){
+            count++
+        }
+    }
+    if(nums[0]<nums[nums.length-1]) count++
+
+    if(count == 1) return true
+    return count <= 1
+    
+};
