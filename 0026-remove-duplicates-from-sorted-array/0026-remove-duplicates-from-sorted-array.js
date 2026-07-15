39291cc2bc1bb71c952d16b13990a43e5591ecde
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-15 12:30:38 +0530

    Time: 1 ms (49.1%), Space: 57.2 MB (73.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.lenght == 1) return 1
-    let j =1 
+    if(nums.length == 0 || nums.length == 1) return nums
+    let j = 1
     for(i=1;i<nums.length;i++){
-        if(nums[i]!== nums[i-1]){
+        if(nums[i]!=nums[i-1]){
             nums[j]=nums[i]
             j++
         }
