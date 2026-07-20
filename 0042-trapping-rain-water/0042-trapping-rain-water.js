e6cabe7abf9dffee3051645184ad868be4872b7b
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-21 00:26:54 +0530

    Time: 0 ms (100%), Space: 55.4 MB (57.62%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let l =0 , r=height.length-1;
+    let lmax = 0; rmax=0
+    let ans = 0
+
+    while(l<r){
+        lmax = Math.max(lmax,height[l])
+        rmax = Math.max(rmax,height[r])
+
+        if(lmax < rmax){
+            ans += lmax-height[l]
+            l++
+        }else{
+            ans += rmax-height[r]
+            r--
+        }
+    }
+    return ans
+};
