94325569aba2bf5ed2e7e751269986fb5aa00426
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-22 01:42:35 +0530

    Time: 1094 ms (5.24%), Space: 71.1 MB (48.51%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    // brute force
+    // let j = 0
+    // let ans = []
+    // while(j<nums.length){
+    //     let sum = 1;
+    //     for(i=0;i<nums.length;i++){
+    //         if(i==j) continue;
+    //         sum = sum * nums[i]
+
+    //     }
+    //     ans.push(sum)
+    //     j++
+    // }
+    // return ans
+
+    // optimal
+    let forwardarr = []
+    let start = 1
+
+    for(i=0;i<nums.length;i++){
+        forwardarr.push(start)
+        start = start*nums[i]
+    }
+
+    let res = []
+    let start2 = 1
+    for(i=nums.length-1;i>=0;i--){
+        res.unshift(start2*forwardarr[i])
+        start2 = start2*nums[i]
+    }
+    return res
+};
