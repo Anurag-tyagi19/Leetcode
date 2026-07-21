abbe1d94c03544ab77bd207028807755fb1823cd
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-22 01:44:46 +0530

    Time: 4 ms (86.28%), Space: 71.3 MB (44.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -18,20 +18,37 @@ var productExceptSelf = function(nums) {
     // }
     // return ans
 
-    // optimal
-    let forwardarr = []
-    let start = 1
+    // // optimal
+    // let forwardarr = []
+    // let start = 1
 
-    for(i=0;i<nums.length;i++){
-        forwardarr.push(start)
-        start = start*nums[i]
+    // for(i=0;i<nums.length;i++){
+    //     forwardarr.push(start)
+    //     start = start*nums[i]
+    // }
+
+    // let res = []
+    // let start2 = 1
+    // for(i=nums.length-1;i>=0;i--){
+    //     res.unshift(start2*forwardarr[i])
+    //     start2 = start2*nums[i]
+    // }
+    // return res
+
+    let n = nums.length
+    let answer = new Array(n).fill(1)
+
+    let left = 1
+    for(let i=0;i<n;i++) {
+        answer[i] = left
+        left *= nums[i]
     }
 
-    let res = []
-    let start2 = 1
-    for(i=nums.length-1;i>=0;i--){
-        res.unshift(start2*forwardarr[i])
-        start2 = start2*nums[i]
+    let right = 1
+    for(let i=n-1;i>=0;i--) {
+        answer[i] *= right
+        right *= nums[i]
     }
-    return res
+
+    return answer
 };
