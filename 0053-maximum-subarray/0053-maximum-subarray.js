71ebaef68b8de28717d66827ded911d4724f5e68
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-07-25 13:10:38 +0530

    Time: 2 ms (72.31%), Space: 63.8 MB (57.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+    let sum = 0
     let maxsum = nums[0]
-    let sum = 0;
 
     for(let i=0;i<nums.length;i++){
-        sum = sum + nums[i]
+        sum = sum+nums[i]
         if(sum>maxsum){
             maxsum = sum
         }
@@ -15,7 +15,5 @@ var maxSubArray = function(nums) {
             sum = 0
         }
     }
-
-
     return maxsum
 };
