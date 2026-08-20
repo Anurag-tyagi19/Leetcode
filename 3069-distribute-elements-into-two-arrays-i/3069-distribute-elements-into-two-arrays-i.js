8733d6dac960fdc805a4a9a4ca0357652b0d64fe
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-08-20 12:07:56 +0530

    Time: 5 ms (12.5%), Space: 56.3 MB (90.63%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -5,7 +5,7 @@
 var resultArray = function(nums) {
     let arr1 = [nums[0]]
     let arr2 = [nums[1]]
-    for(i=2;i<nums.length;i++){
+    for(let i=2;i<nums.length;i++){
         if(arr1[arr1.length-1]>arr2[arr2.length-1]){
             arr1.push(nums[i])
         }else{
