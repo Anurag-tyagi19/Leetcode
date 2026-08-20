7a848c4b341f14598fd72c85a7c3dc3291e711b3
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-08-20 17:45:25 +0530

    Time: 35 ms (34.18%), Space: 68.3 MB (5.36%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    
+    let map = {}
+    map[0] = 1 
+    let sum = 0
+    let count = 0
+    for(let i = 0;i<nums.length;i++){
+
+        sum += nums[i]
+        let check = sum-k
+        if (map[check]) {
+            count += map[check];
+        }
+
+        map[sum] = (map[sum] || 0) + 1;
+    }
+    return count
+
+};
