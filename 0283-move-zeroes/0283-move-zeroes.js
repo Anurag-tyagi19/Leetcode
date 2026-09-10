dfccabc05291c06cdc33a4f073614bc68207f311
Anurag Tyagi <tyagianurag1307@gmail.com>
2026-09-10 11:01:05 +0530

    Time: 2 ms (53.16%), Space: 58.8 MB (90.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let j = 0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] !== 0){
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            j++
+    let k = 0
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            temp = nums[k]
+            nums[k] = nums[i]
+            nums[i] = temp
+            k++
         }
     }
     return nums
